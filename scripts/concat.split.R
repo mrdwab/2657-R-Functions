574eddb79dd639670aa3a9528027a854621c44c0
Ananda Mahto <mrdwab@gmail.com>
2012-04-26 15:11:43 +0530

    Added concat.split, df.sorter, load.scripts.and.data, and row.extractor

---
+++ scripts/concat.split.R
@@ -0,0 +1,75 @@
+concat.split = function(data, split.col, mode=NULL, 
+                        sep=",", drop.col=FALSE) {
+  # Takes a column with multiple values, splits the values into 
+  #   separate columns, and returns a new data.frame.
+  # 'data' is the source data.frame; 'split.col' is the variable that 
+  #   needs to be split; 'mode' can be either 'binary' or 'value' 
+  #   (where 'binary' is default and it recodes values to 1 or NA);
+  #   'sep' is the character separating each value (defaults to ','); 
+  #   and 'drop.col' is logical (whether to remove the original 
+  #   variable from the output or not.
+  #
+  # === EXAMPLES ===
+  #
+  #       dat = data.frame(V1 = c("1, 2, 4", "3, 4, 5", 
+  #                               "1, 2, 5", "4", "1, 2, 3, 5"),
+  #                        V2 = c("1;2;3;4", "1", "2;5", 
+  #                               "3;2", "2;3;4"))
+  #       dat2 = data.frame(V1 = c("Fred, John, Sue", "Jerry, Jill", 
+  #                                "Sally, Ryan", "Susan, Amos, Ben"))
+  #
+  #       concat.split(dat, 1) 
+  #       concat.split(dat, 2, sep=";")
+  #       concat.split(dat, "V2", sep=";", mode="value")
+  #       concat.split(dat, "V1", mode="binary")
+  #       concat.split(dat2, 1)
+  #       concat.split(dat2, "V1", drop.col=TRUE)
+  #
+  # See: http://stackoverflow.com/q/10100887/1270695
+
+  if (is.numeric(split.col)) split.col = split.col
+  else split.col = which(colnames(data) %in% split.col)
+  
+  a = as.character(data[ , split.col])
+  b = strsplit(a, sep)
+  
+  if (suppressWarnings(is.na(try(max(as.numeric(unlist(b))))))) {
+    what = "string"
+    ncol = max(unlist(lapply(b, function(i) length(i))))
+  } else if (!is.na(try(max(as.numeric(unlist(b)))))) {
+    what = "numeric"
+    ncol = max(as.numeric(unlist(b)))
+  }
+  
+  m = matrix(nrow = nrow(data), ncol = ncol)
+  v = vector("list", nrow(data))
+  
+  if (identical(what, "string")) {
+    temp = as.data.frame(t(sapply(b, '[', 1:ncol)))
+    names(temp) = paste(names(data[split.col]), "_", 1:ncol, sep="")
+    temp1 = cbind(data, temp)
+  } else if (identical(what, "numeric")) {
+    for (i in 1:nrow(data)) {
+      v[[i]] = as.numeric(strsplit(a, sep)[[i]])
+    }
+    
+    temp = v
+    
+    for (i in 1:nrow(data)) {
+      m[i, temp[[i]]] = temp[[i]]
+    }
+    
+    m = data.frame(m)
+    names(m) = paste(names(data[split.col]), "_", 1:ncol, sep="")
+    
+    if (is.null(mode) || identical(mode, "binary")) {
+      temp1 = cbind(data, replace(m, m != "NA", 1))
+    } else if (identical(mode, "value")) {
+      temp1 = cbind(data, m)
+    }
+  } 
+  
+  if (isTRUE(drop.col)) temp1[-split.col]
+  else temp1
+  
+}
---
+++ scripts/df.sorter.R
@@ -0,0 +1,43 @@
+df.sorter = function(data, var.order=names(data), col.sort=NULL ) {
+  # Sorts a data.frame by columns or rows or both.
+  # Can also subset the data columns by using 'var.order'.
+  # Can refer to variables either by names or number.
+  # If referring to variable by number, and sorting both the order
+  #   of variables and the sorting within variables, refer to the
+  #   variable numbers of the final data.frame.
+  #
+  # === EXAMPLES ===
+  #
+  #    library(foreign)
+  #    temp = "http://www.ats.ucla.edu/stat/stata/modules/kidshtwt.dta"
+  #    kidshtwt = read.dta(temp); rm(temp)
+  #    df.sorter(kidshtwt, var.order = c("fam", "bir", "wt", "ht"))
+  #    df.sorter(kidshtwt, var.order = c("fam", "bir", "wt", "ht"),
+  #              col.sort = c("birth", "famid")) # USE FULL NAMES HERE
+  #    df.sorter(kidshtwt, var.order = c(1:4),   # DROP THE WT COLUMNS
+  #              col.sort = 3)                   # SORT BY HT1  
+
+  if (is.numeric(var.order)) 
+    var.order = colnames(data)[var.order]
+  else var.order = var.order
+  
+  a = names(data)
+  b = length(var.order)
+  subs = vector("list", b)
+  
+  for (i in 1:b) {
+    subs[[i]] = sort(grep(var.order[i], a, value=TRUE))
+  }
+  x = unlist(subs)
+  y = data[ , x ]
+  
+  if (is.null(col.sort)) {
+    y
+  } else if (is.numeric(col.sort)) {
+    col.sort = colnames(y)[col.sort]
+    y[do.call(order, y[col.sort]), ]
+  } else if (!is.numeric(col.sort)) {
+    col.sort = col.sort
+    y[do.call(order, y[col.sort]), ]
+  }
+}
