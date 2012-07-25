f0b052e3489c39d2f8b2e0fd907622bc9690d246
Ananda Mahto <mrdwab@gmail.com>
2012-07-25 23:22:44 +0530

    added "basic" to multi.freq.table

--- scripts/multi.freq.table.R
+++ scripts/multi.freq.table.R
@@ -1,5 +1,6 @@
 ## @knitr multifreqtable
-multi.freq.table = function(data, sep="", dropzero=FALSE, clean=TRUE) {
+multi.freq.table = function(data, sep="", dropzero=FALSE,
+                            clean=TRUE, basic=FALSE) {
   # Takes boolean multiple-response data and tabulates it according
   #   to the possible combinations of each variable.
   #
@@ -14,19 +15,30 @@ multi.freq.table = function(data, sep="", dropzero=FALSE, clean=TRUE) {
   #   multi.freq.table(dat[1:3], sep="-", dropzero=TRUE)
   #
   # See: http://stackoverflow.com/q/11348391/1270695
+  #      http://stackoverflow.com/q/11622660/1270695
   
-  counts = data.frame(table(data))
-  N = ncol(counts)
-  counts$Combn = apply(counts[-N] == 1, 1, 
-                       function(x) paste(names(counts[-N])[x],
-                                         collapse=sep))
-  if (isTRUE(dropzero)) {
-    counts = counts[counts$Freq != 0, ]
-  } else if (!isTRUE(dropzero)) {
-    counts = counts
+  if(isTRUE(basic)) {
+    counts = data.frame(Freq = colSums(data),
+                        Pct.of.Resp = (colSums(data)/sum(data))*100,
+                        Pct.of.Cases = (colSums(data)/nrow(data))*100)
+  } else if (!isTRUE(basic)) {
+    counts = data.frame(table(data))
+    N = ncol(counts)
+    counts$Combn = apply(counts[-N] == 1, 1, 
+                         function(x) paste(names(counts[-N])[x],
+                                           collapse=sep))
+    counts$Pct.of.Resp = (counts$Freq/sum(data))*100
+    counts$Pct.of.Cases = (counts$Freq/nrow(data))*100
+    if (isTRUE(dropzero)) {
+      counts = counts[counts$Freq != 0, ]
+    } else if (!isTRUE(dropzero)) {
+      counts = counts
+    }
+    if (isTRUE(clean)) {
+      counts = data.frame(Combn = counts$Combn, Freq = counts$Freq, 
+                          Pct.of.Resp = counts$Pct.of.Resp, 
+                          Pct.of.Cases = counts$Pct.of.Cases)
+    }
   }
-  if (isTRUE(clean)) {
-    counts = data.frame(Combn = counts$Combn, Freq = counts$Freq)
-  } 
   counts
 }
