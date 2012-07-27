7d236c2a597e6a035af4bafc4d3dcb3e08768916
Ananda Mahto <mrdwab@gmail.com>
2012-07-27 23:37:35 +0530

    snippets and tips script

---
+++ scripts/snippets.R
@@ -0,0 +1,32 @@
+## USEFUL SNIPPETS
+
+## @knitr scriptsanddata
+load.scripts.and.data = function(path,
+                                 pattern=list(scripts = "*.R$",
+                                              data = "*.rda$|*.Rdata$"), 
+                                 ignore.case=TRUE) {
+  # Reads all the data files and scripts from specified directories.
+  #     In general, should only need to specify the directories.
+  #     Specify directories without trailing slashes.
+  #
+  # === EXAMPLE ===
+  #
+  #    load.scripts.and.data(c("~/Dropbox/Public", 
+  #                            "~/Dropbox/Public/R Functions"))
+    
+  file.sources = list.files(path, pattern=pattern$scripts, 
+                            full.names=TRUE, ignore.case=ignore.case)
+  data.sources = list.files(path, pattern=pattern$data,
+                            full.names=TRUE, ignore.case=ignore.case)
+  sapply(data.sources,load,.GlobalEnv)
+  sapply(file.sources,source,.GlobalEnv)
+}
+
+## @knitr unlistdfs
+unlist.dfs = function(data) {
+  # Specify the quoted name of the source list.
+  q = get(data)
+  prefix = paste0(data, "_", 1:length(q))
+  for (i in 1:length(q)) assign(prefix[i], q[[i]], envir=.GlobalEnv)
+}
+
