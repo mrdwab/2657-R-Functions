78efd9a8d343d7ee94693abafb4b07cea24f5f1c
Ananda Mahto <mrdwab@gmail.com>
2012-12-12 00:58:43 +0530

    Changed @knitr reference

--- scripts/stringseed.sampling.R
+++ scripts/stringseed.sampling.R
@@ -1,6 +1,20 @@
-## @knitr stringseed
+## @knitr stringseedsampling
 
 stringseed.sampling <- function(seedbase, N, n, write.output = FALSE) {
+  # Designed for batch sampling scenarios using alpha-numeric strings
+  #   as a `seedbase`. `N` represents the "population", and `n`, the
+  #   sample size needed. A vector is supplied for each argument (or,
+  #   alternatively, a data.frame with the required information).
+  #   Optionally, the function can write the output of the function
+  #   to a file.
+  #
+  # === EXAMPLE ===
+  #
+  #   stringseed.sampling(seedbase = c("Village 1", "Village 2", "Village 3"),
+  #                       N = c(150, 309, 297), n = c(15, 31, 30))
+  #
+  # See: http://stackoverflow.com/q/10910698/1270695
+  
   require(digest)
   hexval = paste0("0x", sapply(seedbase, digest, "crc32"))
   seeds = type.convert(hexval) %% .Machine$integer.max
